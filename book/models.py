c559ffcd8dc6353a58620e847735d48ceb6a013d
Burak Caglar <caglarih@itu.edu.tr>
2019-06-02 17:53:01 +0300

    store book publishers

--- book/models.py
+++ book/models.py
@@ -5,8 +5,13 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
 
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Book(models.Model):
     name = models.CharField(max_length=256)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     isbn = models.CharField(max_length=13, unique=True)
     page_count = models.PositiveSmallIntegerField()
--- book/views.py
+++ book/views.py
@@ -5,7 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from book.models import Author, Book
+from book.models import Author, Book, Publisher
 
 from functions.product_query import (
     get_product_details,
@@ -26,10 +26,14 @@ class ExploreBookView(View):
             return HttpResponse(status=400)
         details = get_product_details(ProductQueryParameters(isbn, None))
         author, _ = Author.objects.get_or_create(name=details["author"])
+        publisher, _ = Publisher.objects.get_or_create(
+            name=details["publisher"],
+        )
         book = Book.objects.create(
             isbn=details["isbn"],
             name=details["name"],
             author=author,
+            publisher=publisher,
             page_count=details["page_count"],
         )
         return HttpResponse(json.dumps(details), status=201)
